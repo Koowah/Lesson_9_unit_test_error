445d999756aef2f08103ebfe239a536d9edc1968
Koowah <anaslargab@gmail.com>
2022-12-09 14:46:40 +0100

    add vrfcoordinator constructor args

--- hardhat-helper-config.js
+++ hardhat-helper-config.js
@@ -4,14 +4,24 @@ const networkConfig = {
     5: {
         name: "goerli",
         vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D", // VRF coordinator address
+        entranceFee: ethers.utils.parseEther("0.1"),
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+        subscriptionId: 0,
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+    31337: {
+        name: "hardhat",
+        entranceFee: ethers.utils.parseEther("0.1"),
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+        callbackGasLimit: "500000",
+        interval: "30",
     },
 }
+
 const developmentChains = ["hardhat", "dashboard", "localhost", "ganache"]
-const entranceFee = ethers.utils.parseEther("0.1")
 
 module.exports = {
-    entranceFee,
     networkConfig,
     developmentChains,
 }
