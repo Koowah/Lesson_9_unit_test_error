8026a3fb48113eb52e9d1561e7ed5df6006047df
Koowah <anaslargab@gmail.com>
2022-12-09 14:24:55 +0100

    add boilerplate

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,9 +6,35 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.17",
+    networks: {
+        dashboard: {
+            url: "http://localhost:24012/rpc", // truffle dashboard - SECURE
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            chainId: 5,
+            accounts: [PRIVATE_KEY],
+            blockConfirmations: 3,
+        },
+        ganache: {
+            url: "http://127.0.0.1:7545",
+            accounts: {
+                mnemonic:
+                    "crash envelope orbit crash actress debate improve borrow aunt twenty hero base",
+                initialIndex: 0,
+                count: 10,
+            },
+        },
+    },
+
     namedAccounts: {
         deployer: {
             default: 0,
