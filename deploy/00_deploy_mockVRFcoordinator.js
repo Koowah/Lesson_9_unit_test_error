a635f3edead770a48669a066738ae78b04a6e188
Koowah <anaslargab@gmail.com>
2022-12-09 14:46:47 +0100

    ok

--- deploy/00_deploy_mockVRFcoordinator.js
+++ deploy/00_deploy_mockVRFcoordinator.js
@@ -1,20 +1,24 @@
-const { network } = require("hardhat")
-const { entranceFee, developmentChains } = require("./../hardhat-helper-config")
+const { network, ethers } = require("hardhat")
+const { developmentChains } = require("./../hardhat-helper-config")
+
+const BASE_FEE = ethers.utils.parseEther("0.25") // premium - 0.25 link per request
+const GAS_PRICE_LINK = 1e9
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const args = [BASE_FEE, GAS_PRICE_LINK]
 
     if (developmentChains.includes(network.name)) {
         log("Local network detected! Deploying mocks...")
-        await deploy("VRFMock", {
-            // contract: "VRFMock",
+        await deploy("VRFCoordinatorV2Mock", {
+            // contract: "VRFCoordinatorV2Mock",
             from: deployer,
             log: true,
-            args: [entranceFee],
+            args: args,
             waitConfirmations: network.config.blockConfirmations || 1,
         })
-        log("VRFMock deployed!")
+        log("VRFCoordinatorV2Mock deployed!")
         log("-".repeat(50))
     }
 }
