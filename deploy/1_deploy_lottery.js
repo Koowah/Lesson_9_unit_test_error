fd9c1e97ded6153f19b51f1196c2a74421daf146
Koowah <anaslargab@gmail.com>
2022-12-09 14:47:13 +0100

    add chainlink logic onchain and local

--- deploy/1_deploy_lottery.js
+++ deploy/1_deploy_lottery.js
@@ -1,15 +1,43 @@
-const { network } = require("hardhat")
-const { entranceFee } = require("./../hardhat-helper-config")
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("./../hardhat-helper-config")
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("30")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    const currentNetwork = networkConfig[chainId]
+    let vrfCoordinatorV2Address, subscriptionId
 
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        // We get the subscription Id - cf documentation
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait()
+        subscriptionId = transactionReceipt.events[0].args.subId
+        // Fund the subscription - normally need link token
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2Address = currentNetwork["vrfCoordinatorV2"]
+        subscriptionId = currentNetwork["subscriptionId"]
+    }
+
+    const { entranceFee, gasLane, callbackGasLimit, interval } = currentNetwork
+    const args = [
+        vrfCoordinatorV2Address,
+        entranceFee,
+        gasLane,
+        subscriptionId,
+        callbackGasLimit,
+        interval,
+    ]
     await deploy("Lottery", {
         // contract: "Lottery",
         from: deployer,
         log: true,
-        args: [entranceFee],
+        args: args,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
     log("Lottery deployed!")
